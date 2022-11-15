a1303c32910675ca6d6007719b3888bf7f62ca62
nicolasvillarr <nicolasvillar4@gmail.com>
2022-11-15 10:23:45 -0300

    fix

--- client/src/index.js
+++ client/src/index.js
@@ -7,8 +7,7 @@ import { Provider } from 'react-redux'
 import store from './redux/store/index'
 import axios from 'axios';
 
-
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001"
+axios.defaults.baseURL = process.env.REACT_APP_API || "https://pi-country-back-production.up.railway.app"
 
 ReactDOM.render(
   <React.StrictMode>
